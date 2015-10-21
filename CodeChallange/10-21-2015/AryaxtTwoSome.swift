f9a43be297dc86b84f30aea40721be706156fa5b
Aryan <aryaxt@gmail.com>
2015-10-21 10:48:11 -0700

    Initial commit Added twosome files and tests

---
+++ CodeChallange/10-21-2015/AryaxtTwoSome.swift
@@ -0,0 +1,23 @@
+//
+//  Challange.swift
+//  CodeChallange
+//
+//  Created by Aryan on 10/21/15.
+//  Copyright © 2015 iosdevelopers. All rights reserved.
+//
+
+import Foundation
+
+
+// This is the worst and slowest solution to this problem O(n^2), let's try to improve
+public func aryaxtTwoSum(numbers: [Int], targetNumber: Int) -> (Int, Int)? {
+    for i in 0..<numbers.count-1 {
+        for j in 0..<numbers.count {
+            if numbers[i] + numbers[j] == targetNumber {
+                return (i+1, j+1)
+            }
+        }
+    }
+    
+    return nil
+}
