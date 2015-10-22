97c0d7cba549c3d5f69a871fb79e37b8ed239782
Alex Persian <alex_persian@me.com>
2015-10-22 13:24:24 -0500

    Removed unnecessary found variable.

--- CodeChallenge/TwoSome/AlexPersianTwoSum.swift
+++ CodeChallenge/TwoSome/AlexPersianTwoSum.swift
@@ -9,19 +9,15 @@
 import Foundation
 
 public func alexPersianTwoSum(numbers: [Int], target: Int) -> (Int, Int)? {
-    /* The classic while loop + double for loops from into CS */
-    var found = false
-    while (!found) {
+    /* The classic while loop + double for loops from intro to CS */
+    while (true) {
         for (var i: Int = 0; i < numbers.count; i++) {
             for (var j: Int = 0; j < numbers.count; j++) {
                 if ((numbers[i] + numbers[j]) == target) {
-                    found = true
                     return (i + 1, j + 1)
                 }
             }
         }
-        if (!found) {
-            return (-1, -1)
-        }
+        return (-1, -1)
     }
 }
