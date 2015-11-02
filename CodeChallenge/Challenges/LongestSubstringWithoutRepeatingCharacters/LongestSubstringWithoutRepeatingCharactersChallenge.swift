745853e9e34f573e71beeba6481e5e8dc8438b02
Ryan Arana <ryana@darkhorse.com>
2015-11-01 22:01:15 -0800

    Add LongestSubstringWithoutRepeatingCharacters and LetterCombinationsForPhoneNumber challenges

---
+++ CodeChallenge/Challenges/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersChallenge.swift
@@ -0,0 +1,44 @@
+//
+//  LongestSubstringWithoutRepeatingCharactersChallenge.swift
+//  CodeChallenge
+//
+//  Created by Ryan Arana on 11/1/15.
+//  Copyright © 2015 iosdevelopers. All rights reserved.
+//
+
+import Foundation
+
+/*
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Longest Substring Without Repeating Characters
+
+Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+*/
+struct LongestSubstringWithoutRepeatingCharactersChallenge: CodeChallengeType {
+    typealias InputType = String
+    typealias OutputType = Int
+    
+    let title = "Longest Substring Without Repeating Characters"
+    
+    let entries: [CodeChallengeEntry<LongestSubstringWithoutRepeatingCharactersChallenge>] = [
+        // Put your entry instance here!
+    ]
+    
+    func generateDataset() -> [InputType] {
+        return [
+            "abcabcbb",
+            "bbbbb"
+        ]
+    }
+    
+    func verifyOutput(output: OutputType, forInput input: InputType) -> Bool {
+        guard let expected = verificationDictionary[input] else { return false }
+        return output == expected
+    }
+    
+    private let verificationDictionary = [
+        "abcabcbb": 3,
+        "bbbbb": 1
+    ]
+}
---
+++ CodeChallengeTests/Challenges/LongestSubstringWithoutRepeatingCharactersTests.swift
@@ -0,0 +1,9 @@
+
+import XCTest
+@testable import CodeChallenge
+
+class LongestSubstringWithoutRepeatingCharactersTests: CodeChallengeTestCase {
+    func testLetterCombinationsOfPhoneNumberEntries() {
+        runTestsForChallenge(LongestSubstringWithoutRepeatingCharactersChallenge())
+    }
+}
