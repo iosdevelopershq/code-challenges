372798e147e7e2d66c128f70c5a8bf2fcbcb9b52
Jon-Tait Beason <bugkrusha@Jon-Taits-MBP-2.local.tld>
2016-12-13 08:40:24 -0800

    Add instructions for challenge.

---
+++ CodeChallenge/Challenges/TwoSum/Clock.swift
@@ -0,0 +1,18 @@
+//
+//  Clock.swift
+//  CodeChallenge
+//
+//  Created by Jon-Tait Beason on 12/13/16.
+//  Copyright © 2016 iosdevelopers. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Given a time represented as a String, "HH:MM:SS, 
+ calculate the angle the hour hand, minute hand,
+ and the second hand make clockwise from 12:00:00.
+ 
+ The angles should be returned in a tuple as follows
+ (hourHandAngle: Int, minuteHandAngle: Int, secondHandAndle: Int)
+ */
